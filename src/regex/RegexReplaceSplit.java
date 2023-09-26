583f2ebc65ed52de689e8a8de5ec26ba450f5c17
Rafal <sawrafdev@outlook.com>
2023-09-26 20:44:41 +0200

    Added RegexReplaceSplit

---
+++ src/regex/RegexReplaceSplit.java
@@ -0,0 +1,19 @@
+package regex;
+
+public class RegexReplaceSplit {
+    public static void main(String[] args) {
+        //zastepowanie tekstow
+        String text = "The quick brown fox jumps over the lazy dog";
+        String replaced = text.replaceAll("\\w+", "***");
+        System.out.println("Original: " + text);
+        System.out.println("Replaced: " + replaced);
+
+        //splitowanie
+        String text2 = "Rozdzielaj po dowolnym\nbiałym\tznaku";
+        String[] split = text2.split("\\s");
+        for (String s : split) {
+            System.out.println("->" + s);
+        }
+
+    }
+}
